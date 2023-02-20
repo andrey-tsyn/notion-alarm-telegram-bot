2bc944ef532a8441bd61e2f7a09e380cba3b2e9d
hulio13 <andrej.tsyn@yandex.ru>
2023-02-20 22:51:18 +0300

    Create class for inserting variables into template phrase

---
+++ src/main/java/hulio13/notionAlarm/telegramBot/localization/exceptions/InvalidPhraseException.java
@@ -0,0 +1,7 @@
+package hulio13.notionAlarm.telegramBot.localization.exceptions;
+
+public class InvalidPhraseException extends Exception{
+    public InvalidPhraseException(String message) {
+        super(message);
+    }
+}

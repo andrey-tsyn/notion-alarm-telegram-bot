884e1b8a632389ae6d9fd99d62c196f959645c19
hulio13 <andrej.tsyn@yandex.ru>
2023-02-21 10:22:20 +0300

    Change logic of VariablesInPhraseInserter

--- src/main/java/hulio13/notionAlarm/telegramBot/localization/VariablesInPhraseInserter.java
+++ src/main/java/hulio13/notionAlarm/telegramBot/localization/VariablesInPhraseInserter.java
@@ -3,43 +3,16 @@ package hulio13.notionAlarm.telegramBot.localization;
 import java.util.Map;
 import java.util.Set;
 
+// TODO: TEST BEFORE USING IN BOT
 public final class VariablesInPhraseInserter {
-    static public String insert(String phrase, Map<String, String> vars) throws InvalidPhraseException{
-        String[] splitedPhrase = phrase.split("\\{\\{\\|}}");
-        Integer[] indexes = getVarIndexes(phrase);
+    static public String insert(String phrase, Map<String, String> vars){
+        Set<String> keys = vars.keySet();
 
-        for (var index : indexes) {
-            splitedPhrase[index] = vars.get(splitedPhrase[index]);
+        for (var key :
+                keys) {
+            phrase = phrase.replace("{{" + key + "}}", vars.get(key));
         }
 
-        return String.join("", splitedPhrase);
-    }
-
-    static private Integer[] getVarIndexes(String phrase) throws InvalidPhraseException{
-        List<Integer> indexes = new ArrayList<>();
-        int currIndex = 0;
-        boolean wasVar = false, isVarAtThatTime = false;
-
-        for(int i = 0; i < phrase.length(); i++){
-            if (phrase.charAt(i) == '{' && i + 1 < phrase.length()
-            && phrase.charAt(i + 1) == '{'){
-                i += 2;
-                isVarAtThatTime = true;
-                indexes.add(currIndex++);
-            }
-
-            if (phrase.charAt(i) == '}' && i + 1 < phrase.length()
-                    && phrase.charAt(i + 1) == '}'){
-                i += 2;
-                isVarAtThatTime = false;
-                currIndex++;
-            }
-        }
-
-        if (isVarAtThatTime){
-            throw new InvalidPhraseException("Localization template is not correct. Template: " + phrase);
-        }
-
-        return indexes.toArray(new Integer[0]);
+        return phrase;
     }
 }

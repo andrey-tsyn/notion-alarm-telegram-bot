79249c1e4c59d6eb0820563fc0505e5412585c1b
hulio13 <andrej.tsyn@yandex.ru>
2023-02-24 17:32:02 +0300

    Remove unnecesary imports

--- src/main/java/hulio13/notionAlarm/core/registrars/PlannedTaskPlatformRegistrar.java
+++ src/main/java/hulio13/notionAlarm/core/registrars/PlannedTaskPlatformRegistrar.java
@@ -1,9 +1,5 @@
 package hulio13.notionAlarm.core.registrars;
 
-import hulio13.notionAlarm.core.entities.plannedTask.abstraction.PlannedTaskPlatform;
-
-import java.util.HashMap;
-
 public class PlannedTaskPlatformRegistrar {
     static public void registerPlatforms(){
         // TODO: add notion platform

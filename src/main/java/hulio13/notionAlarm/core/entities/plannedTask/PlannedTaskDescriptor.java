72df2c6ae2ad66913309021358651cf4a2769658
hulio13 <andrej.tsyn@yandex.ru>
2023-02-24 23:42:36 +0300

    Update PlannedTaskDescriptor.java

--- src/main/java/hulio13/notionAlarm/core/entities/plannedTask/PlannedTaskDescriptor.java
+++ src/main/java/hulio13/notionAlarm/core/entities/plannedTask/PlannedTaskDescriptor.java
@@ -4,14 +4,10 @@ import hulio13.notionAlarm.core.entities.plannedTask.abstraction.PlannedTaskPlat
 
 public final class PlannedTaskDescriptor {
     public final String linkToDatabase;
-    private PlannedTaskPlatform platform;
-
-    public PlannedTaskDescriptor(String linkToDatabase) {
-        this.linkToDatabase = linkToDatabase;
-    }
+    public final PlannedTaskPlatform platform;
 
     public PlannedTaskDescriptor(String linkToDatabase, PlannedTaskPlatform platform) {
-        this(linkToDatabase);
+        this.linkToDatabase = linkToDatabase;
         this.platform = platform;
     }
 }

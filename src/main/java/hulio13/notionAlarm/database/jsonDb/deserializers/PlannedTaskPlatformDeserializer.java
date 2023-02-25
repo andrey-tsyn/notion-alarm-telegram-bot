5815ee148ab4e9714cd6333fe7edd18864326351
hulio13 <andrej.tsyn@yandex.ru>
2023-02-25 21:52:50 +0300

    Create serializer and deserializer for PlannedTaskPlatform

---
+++ src/main/java/hulio13/notionAlarm/database/jsonDb/deserializers/PlannedTaskPlatformDeserializer.java
@@ -0,0 +1,25 @@
+package hulio13.notionAlarm.database.jsonDb.deserializers;
+
+import com.fasterxml.jackson.core.JacksonException;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
+import hulio13.notionAlarm.core.entities.plannedTask.abstraction.PlannedTaskPlatform;
+import hulio13.notionAlarm.core.repositories.PlannedTaskPlatformRepository;
+
+import java.io.IOException;
+
+public final class PlannedTaskPlatformDeserializer extends StdDeserializer<PlannedTaskPlatform> {
+    protected PlannedTaskPlatformDeserializer() {
+        super(PlannedTaskPlatform.class);
+    }
+
+    @Override
+    public PlannedTaskPlatform deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException, JacksonException {
+        String id = jsonParser.getValueAsString();
+        PlannedTaskPlatform platform = PlannedTaskPlatformRepository.getPlatformById(id);
+        return platform;
+    }
+}
---
+++ src/main/java/hulio13/notionAlarm/database/jsonDb/serializers/PlannedTaskPlatformSerializer.java
@@ -0,0 +1,15 @@
+package hulio13.notionAlarm.database.jsonDb.serializers;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import hulio13.notionAlarm.core.entities.plannedTask.abstraction.PlannedTaskPlatform;
+
+import java.io.IOException;
+
+public final class PlannedTaskPlatformSerializer extends JsonSerializer<PlannedTaskPlatform> {
+    @Override
+    public void serialize(PlannedTaskPlatform platform, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
+        jsonGenerator.writeObject(platform.getId());
+    }
+}

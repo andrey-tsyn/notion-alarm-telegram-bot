97b01d0568302809dbabbd43ce755276bbefa30d
hulio13 <andrej.tsyn@yandex.ru>
2023-02-26 23:23:23 +0300

    Change UserRepository interface methods
    - Remove getUsers and getUserByTelegramId methods
    - Add forEach and getUser with predicate method

--- src/main/java/hulio13/notionAlarm/core/abstractions/UserRepository.java
+++ src/main/java/hulio13/notionAlarm/core/abstractions/UserRepository.java
@@ -3,13 +3,17 @@ package hulio13.notionAlarm.core.abstractions;
 import hulio13.notionAlarm.core.entities.User;
 
 import java.util.List;
+import java.util.function.Consumer;
 import java.util.function.Predicate;
 
 public interface UserRepository {
-    List<User> getUsers();
-    List<User> getUsers(Predicate<User> predicate);
-    User getUserByTelegramId(String telegramId);
+    void forEach(Consumer<User> consumer);
+
+    User getUser(Predicate<User> predicate);
+
     boolean removeUser(User user);
+
     void updateUser(User user);
+
     void addUser(User user);
 }

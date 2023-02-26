950b4f6840e16ed6e468e9aa91ad21e754c7e231
hulio13 <andrej.tsyn@yandex.ru>
2023-02-26 23:20:30 +0300

    Remove redundant throws JacksonException

--- src/main/java/hulio13/notionAlarm/database/jsonDb/deserializers/PlannedTaskPlatformDeserializer.java
+++ src/main/java/hulio13/notionAlarm/database/jsonDb/deserializers/PlannedTaskPlatformDeserializer.java
@@ -3,8 +3,6 @@ package hulio13.notionAlarm.database.jsonDb.deserializers;
 import com.fasterxml.jackson.core.JacksonException;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.DeserializationContext;
-import com.fasterxml.jackson.databind.JavaType;
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
 import hulio13.notionAlarm.core.entities.plannedTask.abstraction.PlannedTaskPlatform;
 import hulio13.notionAlarm.core.repositories.PlannedTaskPlatformRepository;
@@ -17,7 +15,7 @@ public final class PlannedTaskPlatformDeserializer extends StdDeserializer<Plann
     }
 
     @Override
-    public PlannedTaskPlatform deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException, JacksonException {
+    public PlannedTaskPlatform deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException {
         String id = jsonParser.getValueAsString();
         PlannedTaskPlatform platform = PlannedTaskPlatformRepository.getPlatformById(id);
         return platform;

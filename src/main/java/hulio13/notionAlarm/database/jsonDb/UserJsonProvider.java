f75f4746ee72884380b127f98fae098d65039353
hulio13 <andrej.tsyn@yandex.ru>
2023-02-27 20:54:15 +0300

    Create UserJsonProvider.java
    Provider, that working with json files

---
+++ src/main/java/hulio13/notionAlarm/database/jsonDb/UserJsonProvider.java
@@ -0,0 +1,57 @@
+package hulio13.notionAlarm.database.jsonDb;
+
+import hulio13.notionAlarm.database.exceptions.EntityNotFoundException;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public final class UserJsonProvider {
+    private final String pathToFolder;
+
+    public UserJsonProvider(String pathToFolder) {
+        this.pathToFolder = pathToFolder;
+    }
+
+    public String readJsonWithName(String name){
+        Path path = getPathWithName(name);
+
+        if (!path.isAbsolute()){
+            throw new IllegalArgumentException();
+        }
+
+        try {
+            return Files.readString(path);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void writeJsonWithName(String name, String content){
+        Path path = getPathWithName(name);
+
+        try {
+            FileWriter writer = new FileWriter(path.toFile());
+            writer.write(content);
+            writer.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void deleteJsonWithName(String name){
+        try {
+            Files.delete(getPathWithName(name));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private Path getPathWithName(String name){
+        return Paths.get(pathToFolder, name+".json");
+    }
+}

715e12f02961e5e785fc6bd2b17f0a69502d23bb
hulio13 <andrej.tsyn@yandex.ru>
2023-02-27 20:54:08 +0300

    Create UserJsonSerializationProvider

---
+++ src/main/java/hulio13/notionAlarm/database/jsonDb/UserJsonSerializationProvider.java
@@ -0,0 +1,38 @@
+package hulio13.notionAlarm.database.jsonDb;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import hulio13.notionAlarm.core.entities.User;
+import hulio13.notionAlarm.database.exceptions.EntityNotFoundException;
+
+import java.io.*;
+
+public final class UserJsonSerializationProvider {
+    private ObjectMapper objectMapper;
+    public UserJsonSerializationProvider() {
+        objectMapper = new ObjectMapper()
+                .registerModule(new JavaTimeModule())
+                .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+    }
+
+    public String serialize(User user) {
+        try {
+            String str = objectMapper.writeValueAsString(user);
+            return str;
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public User deserialize(String json) throws EntityNotFoundException, IOException {
+        User user = objectMapper.readValue(json, User.class);
+        for (var n :
+                user.getPlannedTasks()) {
+            n.setUserIfNull(user);
+        }
+        return user;
+
+    }
+}

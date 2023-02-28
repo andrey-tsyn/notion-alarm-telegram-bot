43831217f506ed92b51dabece4ba2aebe1fb17a1
hulio13 <andrej.tsyn@yandex.ru>
2023-02-28 20:29:47 +0300

    Remove try-catch block

--- src/main/java/hulio13/notionAlarm/database/jsonDb/providers/UserIdsProvider.java
+++ src/main/java/hulio13/notionAlarm/database/jsonDb/providers/UserIdsProvider.java
@@ -8,16 +8,12 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class UserIdsProvider {
-    static public List<String> getIdsFromDatabaseFolder(String pathToFolder){
-        try {
-            List<String> telegramIds = Files.list(Paths.get(pathToFolder))
-                    .map(Path::toFile)
-                    .filter(file -> file.getName().contains(".json"))
-                    .map(f -> f.getName().replace(".json", ""))
-                    .collect(Collectors.toList());
-            return telegramIds;
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+    static public List<String> getIdsFromDatabaseFolder(String pathToFolder) throws IOException {
+        List<String> telegramIds = Files.list(Paths.get(pathToFolder))
+                .map(Path::toFile)
+                .filter(file -> file.getName().endsWith(".json"))
+                .map(f -> f.getName().replace(".json", ""))
+                .collect(Collectors.toList());
+        return telegramIds;
     }
 }

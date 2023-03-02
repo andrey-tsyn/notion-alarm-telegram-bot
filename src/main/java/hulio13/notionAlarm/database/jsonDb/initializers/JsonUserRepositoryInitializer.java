7d7d74ac92e4ca06071838f4d4a3835852a90c2e
hulio13 <andrej.tsyn@yandex.ru>
2023-03-02 23:05:24 +0300

    Add saving users with interval and save on shutdown

--- src/main/java/hulio13/notionAlarm/database/jsonDb/initializers/JsonUserRepositoryInitializer.java
+++ src/main/java/hulio13/notionAlarm/database/jsonDb/initializers/JsonUserRepositoryInitializer.java
@@ -1,15 +1,32 @@
 package hulio13.notionAlarm.database.jsonDb.initializers;
 
 import hulio13.notionAlarm.core.entities.User;
+import hulio13.notionAlarm.database.jsonDb.io.UserToJsonSaver;
+import hulio13.notionAlarm.database.jsonDb.providers.UserJsonSerializationProvider;
 import hulio13.notionAlarm.database.jsonDb.repositories.JsonUserRepository;
 import hulio13.notionAlarm.database.jsonDb.io.JsonUserRepositoryLoader;
 
 import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public final class JsonUserRepositoryInitializer {
-    static public JsonUserRepository initialize(String databaseFolder){
+    static public JsonUserRepository initialize(String databaseFolder, int delayToSaveInFolder, TimeUnit timeUnit){
         List<User> users = JsonUserRepositoryLoader.loadUsers(databaseFolder);
         JsonUserRepository repository = new JsonUserRepository(users, databaseFolder);
+        UserToJsonSaver saver = new UserToJsonSaver(databaseFolder, repository, new UserJsonSerializationProvider());
+
+        Thread saveThread = new Thread(() -> saver.saveAll());
+
+        ScheduledExecutorService executorService = Executors.newScheduledThreadPool(1);
+        executorService.scheduleAtFixedRate(
+                saveThread,
+                0,
+                delayToSaveInFolder,
+                timeUnit);
+
+        Runtime.getRuntime().addShutdownHook(saveThread);
 
         return repository;
     }

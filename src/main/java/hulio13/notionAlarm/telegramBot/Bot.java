7a5d910389769392ba2b571bc423ed7eafc49345
hulio13 <andrej.tsyn@yandex.ru>
2023-03-04 21:43:14 +0300

    Update Bot.java

--- src/main/java/hulio13/notionAlarm/telegramBot/Bot.java
+++ src/main/java/hulio13/notionAlarm/telegramBot/Bot.java
@@ -20,36 +20,11 @@ public final class Bot extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
-        var button1 = KeyboardButton.builder()
-                .text("Button")
-                .build();
-
-        var button2 = KeyboardButton.builder()
-                .text("'Вназад'")
-                .build();
-
-        var button3 = KeyboardButton.builder().text("Button ponizhe").requestContact(true).build();
-
-        var keyboard = ReplyKeyboardMarkup.builder()
-                .keyboardRow(new KeyboardRow(List.of(button1, button2)))
-                .keyboardRow(new KeyboardRow(List.of(button3)))
-                .build();
-
-        try {
-            execute(
-                    SendMessage.builder()
-                            .text("Some text")
-                            .chatId(update.getMessage().getChatId())
-                            .replyMarkup(keyboard)
-                            .build()
-            );
-        } catch (TelegramApiException e) {
-            throw new RuntimeException(e);
-        }
+        
     }
 
     @Override
     public String getBotUsername() {
-        return BOT_USERNAME;
+        return botUsername;
     }
 }

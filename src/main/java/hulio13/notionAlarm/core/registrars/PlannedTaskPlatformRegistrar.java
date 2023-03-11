a07295b822a6faeb5a45bfdbf0b4e36bceef8523
hulio13 <andrej.tsyn@yandex.ru>
2023-03-11 03:16:40 +0300

    Add notion platform

--- src/main/java/hulio13/notionAlarm/core/registrars/PlannedTaskPlatformRegistrar.java
+++ src/main/java/hulio13/notionAlarm/core/registrars/PlannedTaskPlatformRegistrar.java
@@ -2,10 +2,12 @@ package hulio13.notionAlarm.core.registrars;
 
 import hulio13.notionAlarm.core.emptyImplementations.EmptyPlannedTaskPlatform;
 import hulio13.notionAlarm.core.repositories.PlannedTaskPlatformRepository;
+import hulio13.notionAlarm.notion.NotionPlannedTaskPlatform;
 
 public final class PlannedTaskPlatformRegistrar {
     static public void registerPlatforms(){
         // TODO: add notion platform
         PlannedTaskPlatformRepository.addPlatform(new EmptyPlannedTaskPlatform().getId(), new EmptyPlannedTaskPlatform());
+        PlannedTaskPlatformRepository.addPlatform(new NotionPlannedTaskPlatform().getId(), new NotionPlannedTaskPlatform());
     }
 }

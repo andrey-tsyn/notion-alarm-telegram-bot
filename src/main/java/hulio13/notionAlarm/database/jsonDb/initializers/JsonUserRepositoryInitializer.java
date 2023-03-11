0608fbb13aabb10f2c22f6e0dd45a37edc944cb9
hulio13 <andrej.tsyn@yandex.ru>
2023-03-11 03:01:33 +0300

    JsonRepositoryLoader adapted to work with JsonSerialization interface

--- src/main/java/hulio13/notionAlarm/database/jsonDb/initializers/JsonUserRepositoryInitializer.java
+++ src/main/java/hulio13/notionAlarm/database/jsonDb/initializers/JsonUserRepositoryInitializer.java
@@ -3,21 +3,25 @@ package hulio13.notionAlarm.database.jsonDb.initializers;
 import hulio13.notionAlarm.core.entities.User;
 import hulio13.notionAlarm.database.jsonDb.UsersDumpingScheduler;
 import hulio13.notionAlarm.database.jsonDb.io.UserToJsonSaver;
-import hulio13.notionAlarm.database.jsonDb.providers.UserJsonSerializationProvider;
+import hulio13.notionAlarm.database.jsonDb.serialization.UserJsonSerialization;
 import hulio13.notionAlarm.database.jsonDb.repositories.JsonUserRepository;
-import hulio13.notionAlarm.database.jsonDb.io.JsonUserRepositoryLoader;
+import hulio13.notionAlarm.database.jsonDb.io.JsonRepositoryLoader;
 
-import java.io.IOException;
+import java.util.LinkedList;
 import java.util.List;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 public final class JsonUserRepositoryInitializer {
     static public JsonUserRepository initialize(String databaseFolder, int delayToSaveInFolder, TimeUnit timeUnit){
-        List<User> users = JsonUserRepositoryLoader.loadUsers(databaseFolder);
+
+        List<User> users = JsonRepositoryLoader.loadUsers(databaseFolder, new UserJsonSerialization())
+                .stream()
+                .map(o -> (User) o)
+                .collect(Collectors.toCollection(LinkedList::new));
+
         JsonUserRepository repository = new JsonUserRepository(users, databaseFolder);
-        UserToJsonSaver saver = new UserToJsonSaver(databaseFolder, repository, new UserJsonSerializationProvider());
+        UserToJsonSaver saver = new UserToJsonSaver(databaseFolder, repository, new UserJsonSerialization());
 
         UsersDumpingScheduler.start(saver, delayToSaveInFolder, timeUnit);
 
---
+++ src/main/java/hulio13/notionAlarm/database/jsonDb/io/JsonRepositoryLoader.java
@@ -0,0 +1,42 @@
+package hulio13.notionAlarm.database.jsonDb.io;
+
+import hulio13.notionAlarm.database.jsonDb.providers.JsonFilesProvider;
+import hulio13.notionAlarm.database.jsonDb.providers.JsonProvider;
+import hulio13.notionAlarm.database.jsonDb.serialization.abstractions.JsonSerialization;
+import hulio13.notionAlarm.database.jsonDb.serialization.exceptions.JsonReadException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+
+public final class JsonRepositoryLoader {
+    private static Logger logger = LoggerFactory.getLogger(JsonRepositoryLoader.class);
+    static public List<Object> loadUsers(String pathToFolder, JsonSerialization jsonSerialization){
+        List<String> fileNames = null;
+
+        try{
+            fileNames = JsonFilesProvider.getFileListFromFolder(pathToFolder);
+        }
+        catch (IOException e){
+            logger.error("Access to files failed, maybe no permissions granted or directory does not exist.");
+            throw new RuntimeException(e);
+        }
+
+        List<Object> objects = new LinkedList<>();
+        JsonProvider jsonProvider = new JsonProvider(pathToFolder);
+        for (var fileName :
+                fileNames) {
+            try{
+                objects.add(jsonSerialization.deserialize(jsonProvider.readJsonWithName(fileName)));
+            }
+            catch (JsonReadException e){
+                logger.warn(String.format("Incorrect format of json file or don't have necessary values. Error message: %s",
+                        e.getMessage()));
+            }
+        }
+
+        return objects;
+    }
+}

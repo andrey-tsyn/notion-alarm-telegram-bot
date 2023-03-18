b138eeb4a2c60b9c6dafa34687fa292b72d41d9b
hulio13 <andrej.tsyn@yandex.ru>
2023-03-18 21:57:13 +0300

    Create JsonTgUserPropertiesRepositoryInitializer.java

---
+++ src/main/java/hulio13/notionAlarm/telegramBot/userInfo/database/jsonDb/JsonTgUserPropertiesRepositoryInitializer.java
@@ -0,0 +1,23 @@
+package hulio13.notionAlarm.telegramBot.userInfo.database.jsonDb;
+
+import hulio13.notionAlarm.database.jsonDb.io.JsonRepositoryLoader;
+import hulio13.notionAlarm.database.jsonDb.io.JsonSaver;
+import hulio13.notionAlarm.telegramBot.userInfo.TgUserProperties;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public final class JsonTgUserPropertiesRepositoryInitializer {
+    private static Logger logger = LoggerFactory.getLogger(JsonTgUserPropertiesRepositoryInitializer.class);
+
+    public static JsonTgUserPropertiesRepository initialize(String dbFolder, int delayToSaveInDb, TimeUnit unit){
+        List<TgUserProperties> tgUserProperties = new JsonRepositoryLoader<TgUserProperties>().load(dbFolder, new TgUserPropertiesSerialization());
+
+        JsonTgUserPropertiesRepository repository = new JsonTgUserPropertiesRepository(tgUserProperties);
+        JsonSaver<TgUserProperties> saver = new TgUserPropertiesSaver(dbFolder, repository, new TgUserPropertiesSerialization());
+
+        return null;
+    }
+}

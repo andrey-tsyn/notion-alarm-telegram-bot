74b70cedd993172768bfbeb0d21927d3104e03d1
hulio13 <andrej.tsyn@yandex.ru>
2023-03-18 21:56:57 +0300

    Create TgUserProperties.java

---
+++ src/main/java/hulio13/notionAlarm/telegramBot/userInfo/TgUserProperties.java
@@ -0,0 +1,59 @@
+package hulio13.notionAlarm.telegramBot.userInfo;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public final class TgUserProperties {
+    public final String telegramId;
+    private final String userId;
+    private String handlerId = "DefaultHandler";
+    private String locale;
+    private final Map<String, Object> payload;
+
+    @JsonCreator
+    public TgUserProperties(@JsonProperty("userId") String userId,
+                            @JsonProperty("telegramId") String telegramId,
+                            @JsonProperty("handlerId") String handlerId,
+                            @JsonProperty("locale") String locale,
+                            @JsonProperty("payload") Map<String, Object> payload) {
+        this.userId = userId;
+        this.telegramId = telegramId;
+        this.handlerId = handlerId;
+        this.locale = locale;
+        this.payload = payload;
+    }
+
+    public TgUserProperties(String userId, String telegramId, String handlerId, String locale) {
+        this(userId, telegramId, handlerId, locale, new HashMap<>());
+    }
+
+    public TgUserProperties(String userId, String telegramId, String locale){
+        this(userId, telegramId);
+        this.locale = locale;
+    }
+
+    public TgUserProperties(String userId, String telegramId) {
+        this.userId = userId;
+        this.telegramId = telegramId;
+        this.payload = new HashMap<>();
+    }
+
+    public void setHandlerId(String handlerId) {
+        this.handlerId = handlerId;
+    }
+
+    public void setLocale(String locale) {
+        this.locale = locale;
+    }
+
+    public void clearPayload(){
+        payload.clear();
+    }
+
+    public Map<String, Object> getPayload(){
+        return payload;
+    }
+}

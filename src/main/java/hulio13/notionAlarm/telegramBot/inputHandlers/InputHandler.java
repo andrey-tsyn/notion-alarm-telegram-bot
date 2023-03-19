68753107421fcb61b5a8ae71c32cd67332d2d397
hulio13 <andrej.tsyn@yandex.ru>
2023-03-19 20:39:49 +0300

    Add new method

--- src/main/java/hulio13/notionAlarm/telegramBot/inputHandlers/InputHandler.java
+++ src/main/java/hulio13/notionAlarm/telegramBot/inputHandlers/InputHandler.java
@@ -3,5 +3,7 @@ package hulio13.notionAlarm.telegramBot.inputHandlers;
 import hulio13.notionAlarm.telegramBot.userInfo.TgUserProperties;
 
 public interface InputHandler {
-    Response processInput(String input, TgUserProperties properties);
+    String getId();
+    void processInput(String input, TgUserProperties properties);
+    MessageContainer getMessageForUser();
 }

43da5cf2591583320429c024f7d9d8f3b3824b0c
hulio13 <andrej.tsyn@yandex.ru>
2023-03-19 20:17:35 +0300

    Create InputHandlerRegistrar.java

---
+++ src/main/java/hulio13/notionAlarm/telegramBot/inputHandlers/InputHandlerRegistrar.java
@@ -0,0 +1,17 @@
+package hulio13.notionAlarm.telegramBot.inputHandlers;
+
+import org.reflections.Reflections;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Set;
+
+public final class InputHandlerRegistrar {
+    private static Logger logger = LoggerFactory.getLogger(InputHandlerRegistrar.class);
+
+    public static void registerHandlers(){
+    }
+    }
+}

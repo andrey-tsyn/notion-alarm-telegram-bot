c776dc56e6ef3467ddf8d4780f2d8f99a7160a10
hulio13 <andrej.tsyn@yandex.ru>
2023-03-23 23:53:54 +0300

    Create ResourceFilesProvider.java

---
+++ src/main/java/hulio13/notionAlarm/utils/ResourceFilesProvider.java
@@ -0,0 +1,13 @@
+package hulio13.notionAlarm.utils;
+
+import java.io.File;
+import java.net.URL;
+
+public class ResourceFilesProvider {
+    public static File[] getResourceFolderFiles (String folder) {
+        ClassLoader loader = Thread.currentThread().getContextClassLoader();
+        URL url = loader.getResource(folder);
+        String path = url.getPath();
+        return new File(path).listFiles();
+    }
+}

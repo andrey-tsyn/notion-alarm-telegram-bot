437376388e96a34f78b5fe4b9e57ba1b8dc4020c
hulio13 <andrej.tsyn@yandex.ru>
2023-03-25 18:14:45 +0300

    Many changes in class(Mr. Bad commit)
    - Default language sets with defaultLanguageTag parametr
    - Add logging
    - Add custom exceptions
    - Add getButton method

--- src/main/java/hulio13/notionAlarm/telegramBot/localization/LanguageRepository.java
+++ src/main/java/hulio13/notionAlarm/telegramBot/localization/LanguageRepository.java
@@ -1,47 +1,95 @@
 package hulio13.notionAlarm.telegramBot.localization;
 
+import hulio13.notionAlarm.exceptions.NotInitializedException;
 import hulio13.notionAlarm.telegramBot.localization.entities.Language;
+import hulio13.notionAlarm.telegramBot.localization.exceptions.LanguageTagNotFoundException;
+import hulio13.notionAlarm.telegramBot.localization.exceptions.PhraseNotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.LinkedList;
 import java.util.List;
 
 public final class LanguageRepository {
-    private static Language defaultLang;
-    private static final String DEFAULT_LANGUAGE = "ru-RU";
+    private static final Logger logger = LoggerFactory.getLogger(LanguageRepository.class);
     private static final List<Language> langs = new LinkedList<>();
+    private static Language defaultLang;
+    private static boolean isInitialized = false;
 
-    public static void Init(){
-        // TODO: Some logging
-        if (langs.isEmpty()) langs.addAll(LanguagesProvider.getAll());
 
-        if (langs.contains(DEFAULT_LANGUAGE)){
-            if (defaultLang != null){
-                // TODO: Some logging
-                throw new RuntimeException("Default language tag already assigned");
-            }
-            defaultLang = langs.stream().filter(l -> l.langTag().equals(DEFAULT_LANGUAGE)).findFirst().get();
+    public static void Init(List<Language> languages, String defaultLanguageTag){
+        if(isInitialized){
+
+            return;
+        }
+
+        langs.addAll(languages);
+
+        if (langs.contains(defaultLanguageTag)){
+            defaultLang = langs.stream().filter(l -> l.langTag().equals(defaultLanguageTag)).findFirst().get();
         }
         else{
-            throw new RuntimeException("Default language tag not found");
+            throw new LanguageTagNotFoundException("Default language tag not found");
         }
+
+        logger.info("Language repository is initialized.");
+        isInitialized = true;
     }
+
     public static String getPhrase(String langTag, String phraseId){
-        if (langs.isEmpty()){
-            throw new RuntimeException("Static class is not initialized, call Init() before");
-        }
+        throwExceptionIfNotInitialized();
 
+        Language lang = getLangWithLangTag(langTag);
 
-        Language lang = langs.stream().filter(l -> l.langTag().equals(langTag)).findFirst().get();
-
-        if (lang == null){
-            throw new RuntimeException("Language tag not found");
-        }
+        throwExceptionIfLanguageIsNull(lang, langTag);
 
         String phrase = lang.phrases().get(phraseId);
         if (phrase == null){
-            // TODO: Some logging
-            return defaultLang.phrases().get(phraseId);
+            phrase = defaultLang.phrases().get(phraseId);
+            if (phrase != null){
+
+                return phrase;
+            }
+            throw new PhraseNotFoundException("Phrase not found in default lang and lang " +
+                    "with '" + langTag +"' language tag.");
         }
         return phrase;
+    }
+
+    public static String getButton(String langTag, String buttonId){
+        throwExceptionIfNotInitialized();
+
+        Language lang = getLangWithLangTag(langTag);
+
+        throwExceptionIfLanguageIsNull(lang, langTag);
+
+        String button = lang.buttons().get(buttonId);
+        if (button == null){
+            button = defaultLang.buttons().get(buttonId);
+            if (button != null){
+
+                return button;
+            }
+            throw new PhraseNotFoundException("Button not found in default lang and lang " +
+                    "with '" + langTag +"' language tag.");
+        }
+        return button;
+    }
+
+    private static void throwExceptionIfNotInitialized(){
+        if (!isInitialized){
+            throw new NotInitializedException("Language repository is not initialized, call Init() before.");
+        }
+    }
+
+    private static void throwExceptionIfLanguageIsNull(Language lang, String langTag){
+        if (lang == null){
+            logger.warn("'" + langTag + "' language tag not found.");
+            throw new LanguageTagNotFoundException("Language tag not found");
+        }
+    }
+
+    private static Language getLangWithLangTag(String langTag){
+        return langs.stream().filter(l -> l.langTag().equals(langTag)).findFirst().get();
     }
 }

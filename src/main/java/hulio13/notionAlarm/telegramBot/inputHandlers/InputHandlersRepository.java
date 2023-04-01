cdc720c7c7d88e5d2a52fe15b370ceb8247a0ce1
hulio13 <andrej.tsyn@yandex.ru>
2023-04-01 21:38:36 +0300

    Get default logger if not found with warn

--- src/main/java/hulio13/notionAlarm/telegramBot/inputHandlers/InputHandlersRepository.java
+++ src/main/java/hulio13/notionAlarm/telegramBot/inputHandlers/InputHandlersRepository.java
@@ -1,8 +1,14 @@
 package hulio13.notionAlarm.telegramBot.inputHandlers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.HashMap;
 
 public class InputHandlersRepository {
+    private static final Logger logger =
+            LoggerFactory.getLogger(InputHandlersRepository.class);
+
     private static HashMap<String, InputHandler> inputHandlers = new HashMap<>();
 
     public static void addHandler(String id, InputHandler inputHandler){
@@ -10,6 +16,13 @@ public class InputHandlersRepository {
     }
 
     public static InputHandler getHandlerById(String id){
-        return inputHandlers.get(id);
+        InputHandler handler = inputHandlers.get(id);
+        if (handler != null){
+            return handler;
+        }
+
+        logger.warn("Attempt to access non-existent handler with id: " + id);
+
+        return inputHandlers.get("Default");
     }
 }

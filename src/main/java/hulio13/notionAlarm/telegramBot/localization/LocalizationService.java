c1a3982bb0ee05a970e71c2f9b5c2e27d9e76112
hulio13 <andrej.tsyn@yandex.ru>
2023-04-04 11:34:13 +0300

    Add more detailed error messages + refactoring

--- src/main/java/hulio13/notionAlarm/telegramBot/localization/LocalizationService.java
+++ src/main/java/hulio13/notionAlarm/telegramBot/localization/LocalizationService.java
@@ -4,7 +4,6 @@ import hulio13.notionAlarm.core.Result;
 import hulio13.notionAlarm.exceptions.NotInitializedException;
 import hulio13.notionAlarm.telegramBot.localization.entities.Language;
 import hulio13.notionAlarm.telegramBot.localization.exceptions.LanguageTagNotFoundException;
-import hulio13.notionAlarm.telegramBot.localization.exceptions.NotFoundException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -15,12 +14,12 @@ public final class LocalizationService {
     private static LanguageRepository repository;
     private static boolean isInitialized = false;
 
-    public static void init(LanguageRepository repository){
+    public static void init(LanguageRepository repository) {
         LocalizationService.repository = repository;
         isInitialized = true;
     }
 
-    public static Result<String> getPhraseById(String langTag, String phraseId){
+    public static Result<String> getPhraseById(String langTag, String phraseId) {
         throwExceptionIfNotInitialized();
 
         Language lang = repository.getLanguageByLanguageTag(langTag);
@@ -28,18 +27,19 @@ public final class LocalizationService {
         throwExceptionIfLanguageIsNull(lang, langTag);
 
         String phrase = lang.phrases().get(phraseId);
-        if (phrase == null){
+        if (phrase == null) {
             phrase = repository.getDefaultLang().phrases().get(phraseId);
-            if (phrase != null){
+            if (phrase != null) {
                 return new Result<>(phrase);
             }
-            return new Result<>("not_found", "Phrase not found in default lang and lang " +
-                    "with '" + langTag +"' language tag.");
+            return new Result<>("not_found", "Phrase with phrase id '"
+                    + phraseId + "' not found in default lang and lang " +
+                    "with '" + langTag + "' language tag.");
         }
         return new Result<>(phrase);
     }
 
-    public static Result<String> getButtonById(String langTag, String buttonId){
+    public static Result<String> getButtonById(String langTag, String buttonId) {
         throwExceptionIfNotInitialized();
 
         Language lang = repository.getLanguageByLanguageTag(langTag);
@@ -47,18 +47,19 @@ public final class LocalizationService {
         throwExceptionIfLanguageIsNull(lang, langTag);
 
         String button = lang.buttons().get(buttonId);
-        if (button == null){
+        if (button == null) {
             button = repository.getDefaultLang().buttons().get(buttonId);
-            if (button != null){
+            if (button != null) {
                 return new Result<>(button);
             }
-            return new Result<>("not_found", "Button not found in default lang and lang " +
-                    "with '" + langTag +"' language tag.");
+            return new Result<>("not_found", "Button with button id '" +
+                    buttonId + "' not found in default lang and lang " +
+                    "with '" + langTag + "' language tag.");
         }
         return new Result<>(button);
     }
 
-    public static Result<String> getPhraseByValue(String langTag, String phraseValue){
+    public static Result<String> getPhraseByValue(String langTag, String phraseValue) {
         throwExceptionIfNotInitialized();
 
         Language lang = repository.getLanguageByLanguageTag(langTag);
@@ -66,18 +67,18 @@ public final class LocalizationService {
         throwExceptionIfLanguageIsNull(lang, langTag);
 
         String phrase = lang.phrases().getKey(phraseValue);
-        if (phrase == null){
+        if (phrase == null) {
             phrase = repository.getDefaultLang().phrases().getKey(phraseValue);
-            if (phrase != null){
+            if (phrase != null) {
                 return new Result<>(phrase);
             }
             return new Result<>("not_found", "Phrase not found in default lang and lang " +
-                    "with '" + langTag +"' language tag.");
+                    "with '" + langTag + "' language tag.");
         }
         return new Result<>(phrase);
     }
 
-    public static Result<String> getButtonByValue(String langTag, String buttonValue){
+    public static Result<String> getButtonByValue(String langTag, String buttonValue) {
         throwExceptionIfNotInitialized();
 
         Language lang = repository.getLanguageByLanguageTag(langTag);
@@ -85,25 +86,25 @@ public final class LocalizationService {
         throwExceptionIfLanguageIsNull(lang, langTag);
 
         String button = lang.buttons().getKey(buttonValue);
-        if (button == null){
+        if (button == null) {
             button = repository.getDefaultLang().buttons().getKey(buttonValue);
-            if (button != null){
+            if (button != null) {
                 return new Result<>(button);
             }
             return new Result<>("not_found", "Button not found in default lang and lang " +
-                         "with '" + langTag +"' language tag.");
+                    "with '" + langTag + "' language tag.");
         }
         return new Result<>(button);
     }
 
-    private static void throwExceptionIfNotInitialized(){
-        if (!isInitialized){
+    private static void throwExceptionIfNotInitialized() {
+        if (!isInitialized) {
             throw new NotInitializedException("Language repository is not initialized, call Init() before.");
         }
     }
 
-    private static void throwExceptionIfLanguageIsNull(Language lang, String langTag){
-        if (lang == null){
+    private static void throwExceptionIfLanguageIsNull(Language lang, String langTag) {
+        if (lang == null) {
             logger.warn("'" + langTag + "' language tag not found.");
             throw new LanguageTagNotFoundException("Language tag not found");
         }

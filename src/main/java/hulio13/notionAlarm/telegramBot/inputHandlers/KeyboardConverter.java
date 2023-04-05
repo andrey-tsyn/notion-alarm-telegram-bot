bad1deb64854982107ce6266a081b418783e2afd
hulio13 <andrej.tsyn@yandex.ru>
2023-04-06 01:40:06 +0300

    Create KeyboardConverter.java

---
+++ src/main/java/hulio13/notionAlarm/telegramBot/inputHandlers/KeyboardConverter.java
@@ -0,0 +1,24 @@
+package hulio13.notionAlarm.telegramBot.inputHandlers;
+
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
+
+import java.util.ArrayList;
+
+public class KeyboardConverter {
+    public static ReplyKeyboardMarkup fromStringToTelegram(
+            ArrayList<ArrayList<String>> strKeyboard){
+        var builder = ReplyKeyboardMarkup.builder();
+
+        for (var strRow : strKeyboard) {
+            KeyboardRow row = new KeyboardRow();
+            for (var strBtn : strRow) {
+                row.add(new KeyboardButton(strBtn));
+            }
+            builder.keyboardRow(row);
+        }
+
+        return builder.build();
+    }
+}

7e23ea5d8cd01f2474c368df4a8c681c18944f5a
hulio13 <andrej.tsyn@yandex.ru>
2023-04-06 00:44:33 +0300

    Add interval save in folder

--- src/main/java/hulio13/notionAlarm/telegramBot/tgUserProperties/database/jsonDb/JsonTgUserPropertiesRepositoryInitializer.java
+++ src/main/java/hulio13/notionAlarm/telegramBot/tgUserProperties/database/jsonDb/JsonTgUserPropertiesRepositoryInitializer.java
@@ -1,5 +1,6 @@
 package hulio13.notionAlarm.telegramBot.tgUserProperties.database.jsonDb;
 
+import hulio13.notionAlarm.database.jsonDb.DumpingScheduler;
 import hulio13.notionAlarm.database.jsonDb.io.JsonRepositoryLoader;
 import hulio13.notionAlarm.database.jsonDb.io.JsonSaver;
 import hulio13.notionAlarm.telegramBot.tgUserProperties.TgUserProperties;
@@ -12,7 +13,7 @@ import java.util.concurrent.TimeUnit;
 public final class JsonTgUserPropertiesRepositoryInitializer {
     private static Logger logger = LoggerFactory.getLogger(JsonTgUserPropertiesRepositoryInitializer.class);
 
-    public static JsonTgUserPropertiesRepository initialize(String dbFolder, int delayToSaveInDb, TimeUnit unit){
+    public static void initialize(String dbFolder, int delayToSaveInDb, TimeUnit unit){
         List<TgUserProperties> tgUserProperties = new JsonRepositoryLoader<TgUserProperties>().load(dbFolder, new TgUserPropertiesSerialization());
 
         var repository =
@@ -24,6 +25,6 @@ public final class JsonTgUserPropertiesRepositoryInitializer {
                 new TgUserPropertiesSerialization()
         );
 
-        return null;
+        DumpingScheduler.start(saver, delayToSaveInDb, unit);
     }
 }

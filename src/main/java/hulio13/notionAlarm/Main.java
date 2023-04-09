4c602b16004935734fa708afeded5830e38b9f9d
hulio13 <andrej.tsyn@yandex.ru>
2023-04-09 15:28:06 +0300

    Update Main.java

--- src/main/java/hulio13/notionAlarm/Main.java
+++ src/main/java/hulio13/notionAlarm/Main.java
@@ -10,7 +10,6 @@ import java.util.Map;
 
 public class Main {
     public static void main(String[] args) throws TelegramApiException {
-        // TODO: redo configuration (unsafe)
         // Configuration
         Map<String, Object> configValues = ConfigurationReader.readConfigurationFile("config.json");
         String telegramToken;

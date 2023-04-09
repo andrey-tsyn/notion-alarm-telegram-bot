3688066df65128fef37d9517e49d69bcb11cd977
hulio13 <andrej.tsyn@yandex.ru>
2023-04-09 15:29:03 +0300

    Update Main.java

--- src/main/java/hulio13/notionAlarm/Main.java
+++ src/main/java/hulio13/notionAlarm/Main.java
@@ -1,22 +1,14 @@
 package hulio13.notionAlarm;
 
-import hulio13.notionAlarm.configuration.ConfigurationReader;
-import hulio13.notionAlarm.telegramBot.Bot;
-import org.telegram.telegrambots.meta.TelegramBotsApi;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.util.Map;
 
 public class Main {
-    public static void main(String[] args) throws TelegramApiException {
-        // Configuration
-        Map<String, Object> configValues = ConfigurationReader.readConfigurationFile("config.json");
-        String telegramToken;
-        telegramToken = (String)configValues.get("telegramToken");
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
 
-        // Bot start
-        TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
-        botsApi.registerBot(new Bot(telegramToken));
+    public static void main(String[] args){
+
     }
 }
+

b1ba0864713f01f0856395b19cc58ead466ac116
hulio13 <andrej.tsyn@yandex.ru>
2023-04-14 02:07:18 +0300

    Add logging

--- src/main/java/hulio13/notionAlarm/configuration/reflections/AnnotationFinder.java
+++ src/main/java/hulio13/notionAlarm/configuration/reflections/AnnotationFinder.java
@@ -1,6 +1,8 @@
 package hulio13.notionAlarm.configuration.reflections;
 
 import org.reflections.Reflections;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
@@ -10,12 +12,22 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 public final class AnnotationFinder {
+    private static final Logger logger =
+            LoggerFactory.getLogger(AnnotationsConfiguratorService.class);
+
     public static Set<Class<?>> getClassesWithAnnotation(
             String packageName,
             Class<? extends Annotation> annotation){
         Reflections reflections = new Reflections(packageName);
 
-        return reflections.getTypesAnnotatedWith(annotation);
+        Set<Class<?>> classesAnnotatedWith = reflections.getTypesAnnotatedWith(annotation);
+
+        if (classesAnnotatedWith.size() == 0){
+            logger.warn("Zero classes with annotation found. Is package " +
+                    packageName + "exist?");
+        }
+
+        return classesAnnotatedWith;
     }
 
     public static Set<Field> getFieldsAnnotatedWith(

c74d624b1486ab4dde53236e3180cfb13a926f64
hulio13 <andrej.tsyn@yandex.ru>
2023-05-04 22:58:25 +0300

    Create localization handler

---
+++ src/main/java/hulio13/notionAlarm/telegramBot/inputHandlers/handlers/LocalizationInputHandler.java
@@ -0,0 +1,37 @@
+package hulio13.notionAlarm.telegramBot.inputHandlers.handlers;
+
+import hulio13.notionAlarm.telegramBot.inputHandlers.InputHandler;
+import hulio13.notionAlarm.telegramBot.inputHandlers.InputHandlerMarker;
+import hulio13.notionAlarm.telegramBot.inputHandlers.MessageContainer;
+import hulio13.notionAlarm.telegramBot.inputHandlers.utils.StringsToListsConverter;
+import hulio13.notionAlarm.telegramBot.localization.LocalizationService;
+import hulio13.notionAlarm.telegramBot.tgUserProperties.TgUserProperties;
+
+import java.util.Arrays;
+
+@InputHandlerMarker
+public class LocalizationInputHandler implements InputHandler {
+    @Override
+    public String getId() {
+        return "Localization";
+    }
+
+    @Override
+    public void processInput(String input, TgUserProperties properties) {
+        if (Arrays.asList(LocalizationService
+                .getAvailableLanguageTags())
+                .contains(input)){
+            properties.setLocale(input);
+            properties.setHandlerId("Default");
+        }
+    }
+
+    @Override
+    public MessageContainer getMessageForUser() {
+        return new MessageContainer(
+                "Choose your language.\nВыберите ваш язык.",
+                StringsToListsConverter.convert(4,
+                        LocalizationService.getAvailableLanguageTags())
+        );
+    }
+}

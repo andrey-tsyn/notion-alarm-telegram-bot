19ee573fb2d676e264ce0b22bec89478224cea8a
hulio13 <andrej.tsyn@yandex.ru>
2023-05-07 22:25:14 +0300

    Add HasNonLocalizableText method

--- src/main/java/hulio13/notionAlarm/telegramBot/inputHandlers/InputHandler.java
+++ src/main/java/hulio13/notionAlarm/telegramBot/inputHandlers/InputHandler.java
@@ -4,6 +4,10 @@ import hulio13.notionAlarm.telegramBot.tgUserProperties.TgUserProperties;
 
 public interface InputHandler {
     String getId();
+
+    default boolean HasNonLocalizableText(){return false;}
+
     void processInput(String input, TgUserProperties properties);
+
     MessageContainer getMessageForUser();
 }

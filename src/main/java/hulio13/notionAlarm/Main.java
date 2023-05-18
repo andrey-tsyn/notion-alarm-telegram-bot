e6b42523f5b785a3d99486d6aa6805c32358e386
hulio13 <andrej.tsyn@yandex.ru>
2023-05-18 23:57:57 +0300

    Remove telegram bot dependencies and use start method of bot.

--- src/main/java/hulio13/notionAlarm/Main.java
+++ src/main/java/hulio13/notionAlarm/Main.java
@@ -6,23 +6,16 @@ import hulio13.notionAlarm.configuration.configurators.TelegramConfigurator;
 import hulio13.telegramBoot.Bot;
 import hulio13.telegramBoot.ServiceRegistry;
 import hulio13.telegramBoot.UserIOService;
-import hulio13.telegramBoot.localization.LanguageRepository;
-import hulio13.telegramBoot.localization.LocalizationService;
-import hulio13.telegramBoot.localization.json.JsonLanguagesProvider;
 import hulio13.telegramBoot.tgUserProperties.database.TgUserPropertiesRepository;
 import hulio13.telegramBoot.tgUserProperties.database.jsonDb.JsonTgUserPropertiesRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
-
-import java.io.IOException;
 
 public class Main {
     private static final Logger logger = LoggerFactory.getLogger(Main.class);
 
-    public static void main(String[] args) throws IOException, TelegramApiException {
+    public static void main(String[] args) throws TelegramApiException {
         ConfigurationService.configureWithAnnotations("hulio13", "config.json");
 
         ServiceRegistry.addService(TgUserPropertiesRepository.class,
@@ -32,10 +25,9 @@ public class Main {
 
         Bot bot = ConfiguratorRegistry
                 .get(TelegramConfigurator.class)
-                .getConfiguredBot() ;
+                .getConfiguredBot();
 
-        TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
-        botsApi.registerBot(bot);
+        bot.start();
     }
 }
 
